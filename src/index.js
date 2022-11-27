74dcc708860de0d5fc1d5d6ac1017b8d50612c05
Walter <walter.clark24@yahoo.com>
2022-11-27 01:44:20 -0500

    commit tree 2907ecc0feed7adfd011096f70c26ef8d816ae3f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const init = () => {
-  
+const inputForm = document.querySelector('#form')
+inputForm.addEventListener("submit", (e) => summary(e))
+console.log(summary)
 }
+
+
 
 document.addEventListener('DOMContentLoaded', init);
